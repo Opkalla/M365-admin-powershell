04ce13a5b471701b01c753562c945546a25517fa
Austin Ermish <136514112+FullyRelyOnGod9@users.noreply.github.com>
2023-07-05 13:20:40 -0400

    Create LicenseConversion.ps1

---
+++ LicenseConverstion.ps1
@@ -0,0 +1,13 @@
+# This script is used to change a user from Microsoft 365 E3 to E5
+
+# Connect to Microsoft Graph using 'User' scope
+Connect-MgGraph -Scopes "User.ReadWrite.All"
+
+# Variabes for SKU's for Microsoft 365 E3 and E5
+# Replace SPE_E3 and SPE_E5 with the SKU's you want to use
+$e3Sku = Get-MgSubscribedSku -All | Where SkuPartNumber -eq 'SPE_E3'
+$e5Sku = Get-MgSubscribedSku -All | Where SkuPartNumber -eq 'SPE_E5'
+
+# Combining ADD and REMOVE into one command
+# Replace test@domain with the user you want to change
+Set-MgUserLicense -UserId "test@domain.com" -AddLicenses @{SkuId = $e5Sku.SkuId} -RemoveLicenses @(e3Sku.SkuId)
