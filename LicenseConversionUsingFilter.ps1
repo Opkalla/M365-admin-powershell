81d25d47cee7eb3f65030b23bb071bb56fd0d950
Austin Ermish <136514112+FullyRelyOnGod9@users.noreply.github.com>
2023-08-26 11:08:21 -0400

    Update LicenseConversionUsingFilter.ps1

--- LicenseConversionUsingFilter.ps1
+++ LicenseConversionUsingFilter.ps1
@@ -7,12 +7,12 @@ Connect-MgGraph -Scopes "User.ReadWrite.All"
 # Variables for Microsoft 365 E3 and E5 SkuId's
 # Replace the SkuId with the SkuId of the license you want to filter, and change the variable name if needed
 # Replace the SkuId with the SkuId of the license you want to change to, and change the variable name if needed
-$e3Sku = Get-MgSubscribedSku -All | Where SkuPartNumber -eq 'SPE_E3'
-$e5Sku = Get-MgSubscribedSku -All | Where SkuPartNumber -eq 'SPE_E5'
+$e3Sku = Get-MgSubscribedSku -All | Where-Object SkuPartNumber -eq 'SPE_E3'
+$e5Sku = Get-MgSubscribedSku -All | Where-Object SkuPartNumber -eq 'SPE_E5'
 
 # Replace the filter variable with the variable of the license you want to filter
 # Replace the variables in AddLicenses and RemoveLicenses with the variables you created above
-Get-MgUser -Filter "assignedLicenses/any(x:x/skuId eq $($e3Sku.SkuId) )" | ForEach {Set-MgUserLicense -user $_.UserPrincipalName -AddLicenses @{SkuId = $e5Sku.SkuId} -RemoveLicenses @($e3Sku.SkuId) }
+Get-MgUser -Filter "assignedLicenses/any(x:x/skuId eq $($e3Sku.SkuId) )" | ForEach-Object {Set-MgUserLicense -user $_.UserPrincipalName -AddLicenses @{SkuId = $e5Sku.SkuId} -RemoveLicenses @($e3Sku.SkuId) }
 
 # Disconnect from Microsoft Graph
 Disconnect-MgGraph
