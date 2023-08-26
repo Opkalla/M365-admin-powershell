593ffcb7ed87b55e1ace29896a23b947979893a8
Austin Ermish <136514112+FullyRelyOnGod9@users.noreply.github.com>
2023-08-26 11:06:23 -0400

    Update InviteExternalUser.ps1

--- InviteExternalUser.ps1
+++ InviteExternalUser.ps1
@@ -13,12 +13,19 @@ New-MgInvitation -InvitedUserDisplayName "Test User" -InvitedUserEmailAddress "i
 # Replace inviteduser@domain with the email address of the user you invited
 Get-MgUser -Filter "Mail eq 'inviteduser@domain.com'"
 
+# *Optional*
+# Update usage location
+# The -UserId parameter has the correct format for external users
+# Replace inviteduser_domain with the email address of the user you invited
+# Replace domain.onmicrosoft.com with your tenant domain
+Update-MgUser -UserId 'inviteduser_domain.com#EXT#@domain.onmicrosoft.com' -UsageLocation 'US'
+
 # Variables for Microsoft 365 E5 SkuId and disabled plans
 # In this example, I assign the user an E5 license and disable all service plans except Power BI Pro (BI_AZURE_P2)
 # if you don't need to disable any plans, you can remove the $disabledPlans variable and the DisabledPlans parameter in $addLicenses
 # Replace the SkuId (SPE_E5) with the SkuId of the license you want, and change the variable names if needed
-$e5Sku = Get-MgSubscribedSku -All | Where SkuPartNumber -eq 'SPE_E5'
-$disabledPlans = $e5Sku.ServicePlans | Where ServicePlanName -ne ("BI_AZURE_P2") | Select -ExpandProperty ServicePlanId
+$e5Sku = Get-MgSubscribedSku -All | Where-Object SkuPartNumber -eq 'SPE_E5'
+$disabledPlans = $e5Sku.ServicePlans | Where-Object ServicePlanName -ne ("BI_AZURE_P2") | Select-Object -ExpandProperty ServicePlanId
 
 # Assign license to user with *Optional* disabled plans
 $addLicenses = 
